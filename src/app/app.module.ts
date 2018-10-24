9d10c56a0865d34040b7853993e3fd7a7e805708
User <User@QA-022769.OnBase.net>
2018-10-24 19:14:50 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AlbumHeaderComponent } from './album-header/album-header.component';
 import { PlayButtonComponent } from './play-button/play-button.component';
+import { AlbumComponent } from './album/album.component';
+import { TrackComponent } from './track/track.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlbumHeaderComponent,
-    PlayButtonComponent
+    PlayButtonComponent,
+    AlbumComponent,
+    TrackComponent
   ],
   imports: [
     BrowserModule
